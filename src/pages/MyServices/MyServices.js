c2c9670ad5390a832992f150491d4a1476a4ff87
antudas22 <20antu20@gmail.com>
2022-11-10 16:06:08 +0600

    add my services page

--- src/pages/MyServices/MyServices.js
+++ src/pages/MyServices/MyServices.js
@@ -5,7 +5,11 @@ const MyServices = () => {
     useTitle('My Services')
     return (
         <div>
-            
+            <div className="text-center mb-14">
+        <h3 className="inline-block text-2xl font-bold text-muted border-b-4 border-sky-500">
+          My Services
+        </h3>
+      </div>
         </div>
     );
 };
